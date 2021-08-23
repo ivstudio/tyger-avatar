7e8fbc3fafa93ed9177d75bd48620df0625d59e0
Iggy Villamar <iggy@ivstudio.com>
2021-08-22 20:21:05 -0400

    Add new icons

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+export { default as Chelsea } from './Chelsea'
+export { default as Eric } from './Eric'
+export { default as Samantha } from './Samantha'
+export { default as Torsten } from './Torsten'
