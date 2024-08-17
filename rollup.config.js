5fbc55dfee245fc048fa352c866812063fbc7a80
Iggy Villamar <iggy@ivstudio.com>
2024-08-17 19:47:22 -0400

    add babel to compilation process

--- rollup.config.js
+++ rollup.config.js
@@ -3,6 +3,11 @@ import typescript from 'rollup-plugin-typescript2';
 import css from 'rollup-plugin-css-only';
 import filesize from 'rollup-plugin-filesize';
 import svgr from '@svgr/rollup';
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
+import { babel } from '@rollup/plugin-babel';
+import terser from '@rollup/plugin-terser';
+import { visualizer } from 'rollup-plugin-visualizer';
 
 const mainConfig = {
 	input: 'src/index.ts',
@@ -15,13 +20,22 @@ const mainConfig = {
 	],
 	plugins: [
 		peerDepsExternal(),
+		resolve(),
+		commonjs(),
 		typescript({ useTsconfigDeclarationDir: true }),
+		babel({
+			exclude: 'node_modules/**',
+			extensions: ['.js', '.jsx', '.ts', '.tsx'],
+			babelHelpers: 'bundled',
+		}),
 		filesize(),
 		css({
 			output: 'styles.css',
 			include: ['**/*.css'],
 		}),
 		svgr(),
+		terser(),
+		visualizer({ open: true }),
 	],
 };
 
